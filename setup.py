13ff0059e8b95e30ff48c513c7b02950806ad72a
Anubhav Jain <anubhavj880@gmail.com>
2018-06-12 20:15:16 +0530

    change in setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ with open('README.md') as f:
 with open('LICENSE') as f:
     license = f.read()
 
-setup(name='sample',
+setup(name='demo',
       version='1.0',
       description='Python Distribution Utilities',
       long_description=readme,
       author='Anubhav Jain',
       author_email='anubhavj880@gmail.com',
-      url='https://github.com/anubhavj880/sample',
+      url='https://github.com/anubhavj880/demo',
       license=license,
       packages=['package1', 'package2', 'package1.subpackage1_1', 'package2.subpackage2_1', 'tests'],
       py_modules=['home'],
